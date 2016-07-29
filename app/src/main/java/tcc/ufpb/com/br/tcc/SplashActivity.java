0d35f2c2eeb00ea983011f08495a9cf4aa0f5576
deyvison <deyvison.melo@dce.ufpb.br>
2016-07-29 17:10:07 -0300

    Tela de splash implementada

---
+++ app/src/main/java/tcc/ufpb/com/br/tcc/SplashActivity.java
@@ -0,0 +1,26 @@
+package tcc.ufpb.com.br.tcc;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity implements Runnable{
+
+    private static final int DELAY = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, DELAY);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
