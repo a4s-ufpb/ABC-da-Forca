9c1d68a43af4f1f02bdd072811bfc4a6bb2a9813
deyvison <deyvison.melo@dce.ufpb.br>
2016-10-01 14:03:32 -0300

    layout do jogo responsivo

--- app/src/main/java/tcc/ufpb/com/br/tcc/MainActivity.java
+++ app/src/main/java/tcc/ufpb/com/br/tcc/MainActivity.java
@@ -5,17 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton botaoJogar;
+    ImageView botaoJogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoJogar = (ImageButton) findViewById(R.id.botaoJogar);
+        botaoJogar = (ImageView) findViewById(R.id.botaoJogar);
 
         botaoJogar.setOnClickListener(new View.OnClickListener() {
             @Override
